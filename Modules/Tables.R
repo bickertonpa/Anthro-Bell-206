0560c8d1f148719c5e1ac0567b23cf47135fc48d
bickertonpa <bickerton.patrick@gmail.com>
2025-06-06 06:53:03 -0400

    day one results
    includes day one results and revisions to select() since dplyr and MASS have the same function name.

--- Modules/Tables.R
+++ Modules/Tables.R
@@ -5,6 +5,6 @@ percentile_table <- function(sample_data,sample_percentiles){
   print(common_columns)
   combined_data <- sample_data %>%
   mutate(across(all_of(common_columns), ~ paste0(.x, " (", round(sample_percentiles[[cur_column()]] * 100, 1), "%)"), .names = "{.col} (%)"))
-  combined_data <- combined_data %>% select(-common_columns)
+  combined_data <- combined_data %>% dplyr::select(-common_columns)
   return(combined_data)
 }
